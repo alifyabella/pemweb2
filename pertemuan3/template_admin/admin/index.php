27080ee975ddfec21284ec5f571e2ea7af7d1c93
alifyabella <alifyabella89@gmail.com>
2025-04-16 13:07:20 +0700

    Add files via upload

---
+++ pertemuan3/template_admin/admin/index.php
@@ -0,0 +1,15 @@
+<?php 
+    include_once 'top.php';
+    include_once 'menu.php';
+?>
+<div>
+<div class="container">
+    <h2>Selamat Datang di Halaman Admin</h2>
+    <?php 
+        include_once 'form_registrasi.php';
+    ?>
+</div>
+</div>
+<?php 
+    include_once 'bottom.php';
+?>
